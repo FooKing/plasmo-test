244e6e203c0cac70e127744fe085193451762366
FooKing <skaged@live.co.uk>
2023-03-04 12:16:56 +0000

    JsonViewer Added(PrettyPrint style)
    Made a Json viewer that styles in like a pretty print with various colours for different keys and value types.
    No collapse unsure if we want collapsible yet.

--- src/sidebar/sidebarMainContent/sidebarComponents/jsonTools/JsonEditor/JsonViewer.tsx
+++ src/sidebar/sidebarMainContent/sidebarComponents/jsonTools/JsonEditor/JsonViewer.tsx
@@ -1,20 +1,77 @@
-import { re } from "mathjs";
+import React, { useState } from 'react';
+import testData from './test.json';
+import { copyFromClipboard } from "~Utils/Utils";
+const JsonViewer = () => {
+  const [jsonData, setJsonData] = useState(null);
 
-export default function JsonViewer() {
+  const handleLoadTest = () => {
+    setJsonData(testData);
+  }
+  const handleLoadTest2 = async () => {
+    let res = await fetch('https://dummyjson.com/products');
+    let data = await res.json();
+    setJsonData(data);
+  }
 
+  const handleFromClipboard = async () => {
+    try {
+      let res = await copyFromClipboard()
+      if (res) {
+        let parsed = JSON.parse(res);
+        setJsonData(parsed);
+      }
+    }catch (error) {
+      setJsonData("Invalid Json");
+    }
+  }
+
+  const prettyPrintJson = {
+    toHtml: (thing) => {
+      const htmlEntities = (string) => {
+        return string
+          .replace(/&/g,   '&amp;')
+          .replace(/\\"/g, '&bsol;&quot;')
+          .replace(/</g,   '&lt;')
+          .replace(/>/g,   '&gt;');
+      };
+      const replacer = (match, p1, p2, p3, p4) => {
+        const part =       { indent: p1, key: p2, value: p3, end: p4 };
+        const key =        '<code  class=json-key>';
+        const val =        '<code class=json-value>';
+        const bool =       '<code class=json-boolean>';
+        const str =        '<code class=json-string>';
+        const isBool =     ['true', 'false'].includes(part.value);
+        const valSpan =    /^"/.test(part.value) ? str : isBool ? bool : val;
+        const findName =   /"([\w]+)": |(.*): /;
+        const indentHtml = part.indent || '';
+        const keyName =    part.key && part.key.replace(findName, '$1$2');
+        const keyHtml =    part.key ? key + keyName + '</span>: ' : '';
+        const valueHtml =  part.value ? valSpan + part.value + '</span>' : '';
+        const endHtml =    part.end || '';
+        return indentHtml + keyHtml + valueHtml + endHtml;
+      };
+      const jsonLine = /^( *)("[^"]+": )?("[^"]*"|[\w.+-]*)?([{}[\],]*)?$/mg;
+      return htmlEntities(JSON.stringify(thing, null, 3))
+        .replace(jsonLine, replacer);
+    }
+  };
 
   return(
     <div className="jsonViewerContainer">
-      <div className="jsonViewerContent">
-        This is the Content
-      </div>
+      <div className="jsonViewerContent mockup-code">
+        {jsonData && (
+          <pre dangerouslySetInnerHTML={{__html: prettyPrintJson.toHtml(jsonData)}}></pre>
+        )}
+        </div>
       <div className="jsonViewerFooter">
         <div className="btn-group">
-          <button className="btn btn-sm">Get 2D Json</button>
-          <button className="btn btn-sm">Get 3D Json</button>
-          <button className="btn btn-sm">From Clipboard</button>
+          <button title="Get the current pages 2d Json" className="btn btn-sm" onClick={handleLoadTest}>Get 2D</button>
+          <button title="Get the current pages 3d Json" className="btn btn-sm" onClick={handleLoadTest2}>Get 3D</button>
+          <button title="Get Json from feeder link or a direct json" className="btn btn-sm" onClick={handleFromClipboard}>From Clipboard</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default JsonViewer;
